6295a3b16effbf438b5054dfdb35f91b07c8f01b
hyojeong-commits <kweunsungo@gmail.com>
2026-05-27 16:20:27 +0900

    Update 3-5.c

--- code/3-5.c
+++ code/3-5.c
@@ -3,26 +3,21 @@
 int main(int argc, const char* argv[]) {
     const char* filename = "numbers.txt";
 
-    FILE* fp = fopen(filename, "r");
-    if (fp == NULL) {
+    FILE* fp =fopen(filename, "r");
+    if (fp ==NULL) {
         return 0;
     }
-    int sum = 0;
+    int sum =0;
     char buf[100];
     int num;
 
     while (fgets(buf, sizeof(buf), fp) != NULL) {
-       if (sscanf(buf, "%d", &num) == 1) {
-           sum += num;
+       if (sscanf(buf, "%d", &num) ==1) {
+           sum +=num;
        }else {
            fprintf(stderr, "invalid input %s", buf);
        }
    }
-
-
-    int sum = 0;
-
-
     printf("sum: %d\n", sum);
     fclose(fp);
     return 0;
