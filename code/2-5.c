0c3acc3dd625bc39865e66be9aabbe331dc83c32
hyojeong-commits <kweunsungo@gmail.com>
2026-05-27 17:11:11 +0900

    Update 2-5.c

--- code/2-5.c
+++ code/2-5.c
@@ -3,7 +3,17 @@
 int main(int argc, const char* argv[]) {
     if (argc < 3)
         return 0;
+ FILE* fp_src = fopen(argv[1], "r");
+    FILE* fp_dst = fopen(argv[2], "w");
 
+    char buf[1024];
+    while (fgets(buf, sizeof(buf), fp_src) != NULL) {
+        fputs(buf, fp_dst);
+    }
 
+    fclose(fp_src);
+    fclose(fp_dst);
 }
+
+
 
