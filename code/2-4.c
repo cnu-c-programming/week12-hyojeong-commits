12964fb82cd356d614062ba40a5b33a9f8f9d2e3
hyojeong-commits <kweunsungo@gmail.com>
2026-05-28 17:20:52 +0900

    Update 2-4.c

--- code/2-4.c
+++ code/2-4.c
@@ -5,7 +5,17 @@ int main(int argc, const char* argv[]) {
         return 0;
 
     FILE* fp = fopen(argv[1], "r");
+    int freq[26] ={0};
+    char ch;
 
+    while (!feof(fp)){
+        fscanf(fp, "%c", &ch);
+        if (ch >='a' && ch <='z')
+            freq[ch -'a']++;
+    }
+
+    for (int i =0; i <26; i++)
+        printf("%c: %d\n", 'a' +i, freq[i]);
 
 
     fclose(fp);
