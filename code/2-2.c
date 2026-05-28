1f5d92f1679a90d35ded43cbf2139e2df727dec5
hyojeong-commits <kweunsungo@gmail.com>
2026-05-28 16:13:57 +0900

    Update 2-2.c

--- code/2-2.c
+++ code/2-2.c
@@ -8,32 +8,25 @@ typedef struct student {
 
 int main(int argc, const char* argv[]) {
     FILE* fp = fopen("student.txt", "r");
-    if (fp == NULL) return 0;
-
     int count = 0;
     Student students[64];
 
-    while (fscanf(fp, "%s %d", students[count].name, &students[count].score) != EOF) {
-        count++;
-        if (count >= 64) break; // 배열 범위 초과 방지
-    }
+    while (!feof(fp)) {
+       fscanf(fp, "%s %d", students[count].name, &students[count].score);
+       count++;
+   }
 
-    if (count == 0) {
-        fclose(fp);
-        return 0;
-    }
+    int max=0;
+    float avg =0;
+    int sum =0;
 
-    int ma
-    float avg = 0;
-    int sum = 0;
-
-    for (int i = 0; i < count; i++) {
+    for (int i =0; i <count; i++) {
         if (students[i].score > max) {
-            max = students[i].score;
+            max =students[i].score;
         }
-        sum += students[i].score;
+        sum +=students[i].score;
     }
-    avg = (float)sum / count;
+    avg =(float)sum /count;
 
     printf("max: %d\n", max);
     printf("avg: %.2f\n", avg);
