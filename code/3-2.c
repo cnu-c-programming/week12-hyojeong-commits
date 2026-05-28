ebb9df076cc4e6e2615fbbb8c05970e3ab4eaae4
hyojeong-commits <kweunsungo@gmail.com>
2026-05-28 23:10:23 +0900

    Update 3-2.c

--- code/3-2.c
+++ code/3-2.c
@@ -12,7 +12,7 @@ int main(int argc, const char* argv[]) {
     if (fp==NULL)
         return 0;
 
-    fgets(buffer, sizeof(buffer), fp);    fclose(fp);
+    fgets(buffer, sizeof(buffer), fp);   
     fclose(fp);
     printf("%s\n", buffer);
 
